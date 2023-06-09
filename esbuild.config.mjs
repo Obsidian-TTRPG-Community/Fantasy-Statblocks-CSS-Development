cfb6566d22538dcbf399ec2090fe53d8d6289d1f
Sigrunixia <Scholarlysigrun@icloud.com>
2023-06-09 16:10:01 -0500

    :recycle: Refactor(Root): Updated File Structure to Accomodate Multiple CSS Files

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -3,9 +3,10 @@
 	Config based on @jdanielmourao's Sanctum theme esbuild setup.
 	(https://github.com/jdanielmourao/obsidian-sanctum)
 	Further based on https://github.com/damiankorcz/Prism-Theme/blob/main/esbuild.config.mjs
+	Updated to Esbuild 17 by @Sigrunixia
 */
 
-import esbuild from "esbuild";
+import * as esbuild from "esbuild";
 import { config } from "dotenv";
 import { sassPlugin } from "esbuild-sass-plugin";
 import time from 'esbuild-plugin-time';
@@ -16,25 +17,64 @@ const prod = process.argv[2] === "production";
 const dir = "./";
 
 /** Paths for final file */
-const fileProd = `${dir}/Pathfinder2E-TTRPG-Statblock-CSS.css`;
-const fileDev = `${dir}/Pathfinder2E-TTRPG-Statblock-CSS-DEV.css`;
+/** PF2E */
+const pf2efileProd = `${dir}/Basic-Pathfinder-2e-Layout.css`;
+const pf2efileDev = `${dir}/Basic-Pathfinder-2e-Layout-DEV.css`;
 
-esbuild.build({
-	/** Entry point should be where everything is imported into. */
-	entryPoints: ["index.scss"],
+const buildPF2E = async () => {
+	const context = await esbuild.build({
+		/** Entry point should be where everything is imported into. */
+		entryPoints: ["pf2e-index.scss"],
+		logLevel: "info",
+		outfile: prod ? pf2efileProd : pf2efileDev,
+		// minify: true,
+		plugins: [
+			sassPlugin({
+				cache: true,
+				charset: false,
+				alertColor: true,
+				alertAscii: true,
+			}),
+			time()
+		]
+	});
 
-	/** npm run dev will watch for file changes and rebuild instantly. */
-	watch: !prod,
-	logLevel: "info",
-	outfile: prod ? fileProd : fileDev,
-	// minify: true,
-	plugins: [
-		sassPlugin({
-			cache: true,
-			charset: false,
-			alertColor: true,
-			alertAscii: true,
-		}),
-		time()
-	]
-}).catch(() => process.exit(1));
+	// Enable watch mode
+	if (!prod) {
+		await context.rebuild();
+		await context.watch();
+	}
+};
+
+/** BNB Bestiary */
+const bnbfileProd = `${dir}/BnB-Layout.css`;
+const bnbfileDev = `${dir}/BnB-Layout-DEV.css`;
+
+const buildBNB = async () => {
+	const context = await esbuild.build({
+		/** Entry point should be where everything is imported into. */
+		entryPoints: ["bnb-index.scss"],
+		logLevel: "info",
+		outfile: prod ? bnbfileProd : bnbfileDev,
+		minify: true,
+		plugins: [
+			sassPlugin({
+				cache: true,
+				charset: false,
+				alertColor: true,
+				alertAscii: true,
+			}),
+			time()
+		]
+	});
+
+	// Enable watch mode
+	if (!prod) {
+		await context.rebuild();
+		await context.watch();
+	}
+};
+
+// Call the build functions
+buildPF2E().catch(() => process.exit(1));
+buildBNB().catch(() => process.exit(1));
